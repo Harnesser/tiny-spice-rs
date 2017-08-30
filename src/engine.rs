976fdeff415abac0283ae536986ed0d86e194e8c
Marty <harnesser+kartoffel@gmail.com>
2017-08-30 20:03:05 +0100

    first-cut voltage matrix and current vector construction

--- src/engine.rs
+++ src/engine.rs
@@ -35,25 +35,38 @@ impl Engine {
         println!("*INFO* Building Voltage Node Matrix and Current Vector");
 
         // Voltage Node Matrix
-        type AdjacencyList = Vec<circuit::NodeId>;
-        let mut adj_matrix: Vec<AdjacencyList> = vec![ vec![] ];
+        // I think I have to make this out of Vecs (on the heap) because c_nodes is
+        // not known at compile time. Makes sense, I suppose - could blow the stack if
+        // c_nodes is any way huge.
+        let mut v = vec![ vec![0.0; c_nodes]; c_nodes];
+        let mut i = vec![0.0; c_nodes];
 
-/*
 
+        // Fill up the voltage node and current vector
+        // This needs to know about each of the kinds of circuit elements, so
+        // the node equations can be built up appropriately.
         for el in &ckt.elements {
             match *el {
                 circuit::Element::I(circuit::CurrentSource{ ref p, ref n, ref value }) => {
-
-                    println!("ADSF");
+                    println!("  [ELEMENT] Current source: {} into node {} and out of node {}",
+                            value, p, n);
+                    i[*p] = i[*p] + value; // += doesn't work here
+                    i[*n] = i[*n] - value;
                 }
-                _ => {
-                    println!("Ignoring");
+                circuit::Element::R(circuit::Resistor{ ref a, ref b, ref value }) => {
+                    println!("  [ELEMENT] Resistor");
+                    let over = 1.0 / value;
+                    v[*a][*a] = v[*a][*a] + over;
+                    v[*a][*b] = v[*a][*b] - over;
+                    v[*b][*a] = v[*b][*a] + over;
+                    v[*b][*b] = v[*b][*b] - over;
                 }
+                
             }
         }
-*/
 
-
+        println!("{:?}", i);
+        println!("{:?}", v);
     }
 
 }
