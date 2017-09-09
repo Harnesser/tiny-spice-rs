7cc407d0bbb40bea70b69472393cfabb2bc610bc
Marty <harnesser+kartoffel@gmail.com>
2017-09-09 22:25:46 +0100

    diode - more tests, reverse bias and stuff

---
+++ tests/test_irdvv.rs
@@ -0,0 +1,49 @@
+extern crate tiny_spice;
+
+use tiny_spice::circuit::*;
+use tiny_spice::engine;
+
+mod common;
+use common::assert_nearly;
+
+#[test]
+fn test() {
+    engine::banner();
+
+    let mut eng = engine::Engine::new();
+    let ckt = build();
+    let v = eng.dc_operating_point(&ckt);
+    println!("\n*INFO* Done");
+
+    assert_nearly(v[1], 0.73217);
+    assert_nearly(v[2], 0.73217); // 0V source
+    assert_nearly(v[3], 0.73217); // 0V source
+    assert_nearly(v[4] + v[5], 3.0); // branch current
+}
+
+
+fn build() -> Circuit {
+    let mut ckt = Circuit::new();
+    ckt.elements.push(
+        Element::I(CurrentSource{p: 0, n: 1, value: 3.0}),
+    );
+
+    // R with series 0V source to measure branch current
+    ckt.elements.push(
+        Element::V(VoltageSource{p: 1, n: 2, value: 0.0}),
+    );
+
+    ckt.elements.push(
+        Element::R(Resistor{a: 2, b: 0, value: 10.0}),
+    );
+
+    // D with series 0V source to measure branch current
+    ckt.elements.push(
+        Element::V(VoltageSource{p: 1, n: 3, value: 0.0}),
+    );
+    ckt.elements.push(
+        Element::D(Diode{p: 3, n: 0, i_sat: 1e-9, tdegc: 27.0}),
+    );
+    ckt
+}
+
---
+++ tests/test_v_d_vs_d.rs
@@ -0,0 +1,33 @@
+extern crate tiny_spice;
+
+use tiny_spice::circuit::*;
+use tiny_spice::engine;
+
+mod common;
+use common::assert_nearly;
+
+#[test]
+fn test() {
+    engine::banner();
+
+    let mut eng = engine::Engine::new();
+    let ckt = build();
+    let v = eng.dc_operating_point(&ckt);
+    println!("\n*INFO* Done");
+
+    assert_nearly(v[1], 5.0);
+}
+
+
+// Voltage source with a set of diodes
+// (1) V --> D1 (fwd) (2) --> D2(rev) (0)
+fn build() -> Circuit {
+    let mut ckt = Circuit::new();
+    ckt.elements.push( Element::V(VoltageSource{p: 1, n: 0, value: 5.0}) );
+    // 'forward' biased
+    ckt.elements.push( Element::D(Diode{p: 1, n: 2, i_sat: 1e-9, tdegc: 27.0}) );
+    // reversed bias
+    ckt.elements.push( Element::D(Diode{p: 0, n: 2, i_sat: 1e-9, tdegc: 27.0}) );
+    ckt
+}
+
