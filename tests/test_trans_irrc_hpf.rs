99cc215db0ba477c60d5d42cf0c7d393280956a3
Marty <harnesser+kartoffel@gmail.com>
2017-09-16 22:40:48 +0100

    trans - more cap tests - HPF

---
+++ tests/test_trans_irrc_hpf.rs
@@ -0,0 +1,78 @@
+extern crate tiny_spice;
+
+use tiny_spice::circuit::*;
+use tiny_spice::engine;
+
+mod common;
+use common::assert_nearly;
+
+#[test]
+fn test_irrc_trans_hpf_1kHz() {
+    engine::banner();
+
+    let mut eng = engine::Engine::new();
+    let ckt = build(1.0e3);
+    let v = eng.transient_analysis(&ckt, "waves/trans_irrc_hpf_1kHz.dat");
+    println!("\n*INFO* Done");
+
+    assert!(false);
+}
+
+#[test]
+fn test_irrc_trans_hpf_2kHz() {
+    engine::banner();
+
+    let mut eng = engine::Engine::new();
+    let ckt = build(2.0e3);
+    let v = eng.transient_analysis(&ckt, "waves/trans_irrc_hpf_2kHz.dat");
+    println!("\n*INFO* Done");
+
+    assert!(false);
+}
+
+#[test]
+fn test_irrc_trans_hpf_5kHz() {
+    engine::banner();
+
+    let mut eng = engine::Engine::new();
+    let ckt = build(5.0e3);
+    let v = eng.transient_analysis(&ckt, "waves/trans_irrc_hpf_5kHz.dat");
+    println!("\n*INFO* Done");
+
+    assert!(false);
+}
+
+#[test]
+fn test_irrc_trans_hpf_10kHz() {
+    engine::banner();
+
+    let mut eng = engine::Engine::new();
+    let ckt = build(10.0e3);
+    let v = eng.transient_analysis(&ckt, "waves/trans_irrc_hpf_10kHz.dat");
+    println!("\n*INFO* Done");
+
+    assert!(false);
+}
+
+
+fn build( freq: f32 ) -> Circuit {
+    let mut ckt = Circuit::new();
+
+    // 10V Voltage Source
+    ckt.elements.push(
+        Element::Isin(CurrentSourceSine{p: 0, n: 1, vo: 0.0, va: 10.0, freq: freq}),
+    );
+    ckt.elements.push(
+        Element::R(Resistor{a: 1, b: 0, value: 1.0}),
+    );
+
+    // High-pass filter - 5kHz cut-off
+    ckt.elements.push(
+        Element::C(Capacitor{a: 1, b: 2, value: 0.032e-6}),
+    );
+    ckt.elements.push(
+        Element::R(Resistor{a: 2, b: 0, value: 1.0e3}),
+    );
+    ckt
+}
+
