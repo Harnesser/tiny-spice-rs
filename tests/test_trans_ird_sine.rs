0532f5116eff58eeafbdb3386057615ba5d169f3
Marty <harnesser+kartoffel@gmail.com>
2017-10-18 22:51:57 +0100

    test - updating single-diode transient testcases

--- tests/test_trans_ird_sine.rs
+++ tests/test_trans_ird_sine.rs
@@ -3,46 +3,45 @@ extern crate tiny_spice;
 use tiny_spice::circuit::*;
 use tiny_spice::engine;
 
-mod common;
-use common::assert_nearly;
 
 #[test]
+#[allow(non_snake_case)]
 fn test_trans_ir_sine_1kHz_10us() {
 
     let mut eng = engine::Engine::new();
     eng.TSTEP = 10e-6;
-    let ckt = build(1e3);
-    let v = eng.transient_analysis(&ckt, "waves/trans_ird_sine_1kHz_10us.dat");
+    let ckt = build(2.0, 1e3, 1e-9);
+    let stats = eng.transient_analysis(&ckt, "waves/trans_ird_sine_1kHz_10us.dat");
     println!("\n*INFO* Done");
-
-    assert!(false);
+    assert!(stats.end >= eng.TSTOP);
 }
 
 
 #[test]
+#[allow(non_snake_case)]
 fn test_trans_ir_sine_1kHz_1us() {
 
     let mut eng = engine::Engine::new();
     eng.TSTEP = 1e-6;
-    let ckt = build(1e3);
-    let v = eng.transient_analysis(&ckt, "waves/trans_ird_sine_1kHz_1us.dat");
+    let ckt = build(2.0, 1e3, 1e-9);
+    let stats = eng.transient_analysis(&ckt, "waves/trans_ird_sine_1kHz_1us.dat");
     println!("\n*INFO* Done");
-
-    assert!(false);
+    assert!(stats.end >= eng.TSTOP);
 }
 
 
 
-fn build( freq: f32 ) -> Circuit {
+fn build( amp: f32, freq: f32, isat: f32 ) -> Circuit {
+    let i_offset = 0.0;
     let mut ckt = Circuit::new();
     ckt.elements.push(
-        Element::Isin(CurrentSourceSine{p: 0, n: 1, vo: 3.0, va: 1.0, freq: freq}),
+        Element::Isin(CurrentSourceSine{p: 0, n: 1, vo: i_offset, va: amp, freq: freq}),
     );
     ckt.elements.push(
         Element::R(Resistor{a: 1, b: 0, value: 10.0}),
     );
     ckt.elements.push(
-        Element::D(Diode{p: 1, n: 0, i_sat: 1e-9, tdegc: 27.0}),
+        Element::D(Diode{p: 1, n: 0, i_sat: isat, tdegc: 27.0}),
     );
     ckt
 }
