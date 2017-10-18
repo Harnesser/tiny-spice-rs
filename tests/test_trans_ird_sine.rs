f82db281281b6749ff9ff77a49340a90c10d19a2
Marty <harnesser+kartoffel@gmail.com>
2017-10-18 23:06:59 +0100

    test - add loop() to single-diode testcase

--- tests/test_trans_ird_sine.rs
+++ tests/test_trans_ird_sine.rs
@@ -29,6 +29,53 @@ fn test_trans_ir_sine_1kHz_1us() {
     assert!(stats.end >= eng.TSTOP);
 }
 
+#[test]
+#[ignore]
+#[allow(non_snake_case)]
+fn test_trans_ir_sine_loop() {
+
+    let timesteps = [10e-6, 5e-6, 2e-6, 1e-6];
+    let amps = [-2.0, -1.0, -0.5, 0.5, 1.0, 2.0];
+    let freqs = [3.0e3, 2.5e3, 2.0e3, 1.0e3, 0.5e3, 0.4e3, 0.3e3, 0.2e3, 0.1e3, 0.05e3];
+    let isats = [1e-9, 1e-12, 1e-13];
+/*
+    let timesteps = [10e-6, 1e-6];
+    let amps = [-2.0, 2.0];
+    let freqs = [10.0e3, 0.1e3];
+    let isats = [1e-9, 1e-13];
+*/
+
+    let mut i = 0;
+    let mut fails = 0;
+    for timestep in timesteps.iter() {
+        for freq in freqs.iter() {
+            for amp in amps.iter() {
+                for isat in isats.iter() {
+                    let mut eng = engine::Engine::new();
+                    eng.TSTEP = *timestep;
+                    eng.TSTOP = 2.0e-3;
+
+                    let specs = format!("{:04} {} {} {} {}", i, timestep, amp, freq, isat);
+                    println!("LOOP-SPEC {}", specs);
+                    let ckt = build(*amp, *freq, *isat);
+
+                    let filename = format!("waves/test_trans_ird_sine_loop/{:04}.dat", i);
+                    let stats = eng.transient_analysis(&ckt, &filename);
+                    println!("{}", stats);
+                    if stats.end >= eng.TSTOP {
+                        println!("LOOP-RESULT {} GOOD\n\n", specs);
+                    } else {
+                        println!("LOOP-RESULT {} BAD\n\n", specs);
+                        fails += 1;
+                    }
+
+                    i += 1;
+                }
+            }
+        }
+    }
+    assert!(fails == 0);
+}
 
 
 fn build( amp: f32, freq: f32, isat: f32 ) -> Circuit {
